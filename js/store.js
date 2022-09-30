b2290bb28ed2c4964991b120194739b9aab6d477
Wouldee <114445716+Wouldee@users.noreply.github.com>
2022-09-30 18:29:22 +1300

    Update store.js
    worked on client storage

--- js/store.js
+++ js/store.js
@@ -6,7 +6,7 @@ localStorage.getItem("name");
 localStorage.removeItem("name");
 
 Compliated:
-request = window.indexedDB.open("database_name",1); // creates asynchronous db request
+request = window.indexedDB.open("database_name",1); // creates asynchronous db request, second argument is version
 
 // first time:
 request.addEventListener("upgradeneeded", (e) => {
@@ -52,7 +52,7 @@ table FACE
 column GAME_ID int
 column TILE_X int
 column TILE_Y int
-column ID int
+column LINK_ID int
 column DIRECTION string
 
 table RECORD
@@ -71,3 +71,61 @@ request.result contains the database handle
 
 
 */
+
+// read and write to storage on the client machine
+function ClientStore () {
+
+	// connect to the local database
+	var request = window.indexedDB.open("database_name",1);
+
+	// first time
+	request.addEventListener("upgradeneeded", this.create_database(e));
+
+	//...
+	request.addEventListener("error" () => console.error("Database failed to open"))
+	request.addEventListener("success" () => console.log("Database opened"))
+}
+
+ClientStore.prototype.create_database = function (e) {
+	// create database
+	var database = e.target.result;
+
+	// GAME - identifies an individual level, the options used to create it, and the current scroll position
+	// unique index is generated based on options
+	var gameStore = database.createObjectStore("GAME");
+	gameStore.createIndex("ID", "ID", { unique: true });
+	gameStore.createIndex("LEVEL", "LEVEL", { unique: false });
+	gameStore.createIndex("OPTIONS", ["TILING", "SIZE", "X_CONTINUOUS", "Y_CONTINUOUS", "SOURCES", "RED", "GREEN", "BLUE", "GROWTH", "MIX", "FILTER", "PRISM"], { unique: false });
+	gameStore.createIndex("SCROLL", ["X_OFFSET", "Y_OFFSET"], { unique: false });
+
+	// TILE - an individual tile on a game
+	var tileStore = database.createObjectStore("TILE");
+	tileStore.createIndex("TILE_ID", ["GAME_ID", "X", "Y"], { unique: true });
+	tileStore.createIndex("PROPERTIES", ["SHAPE", "TYPE", "FORM", "COLOUR"], { unique: false });
+
+	// FACE - a face
+table FACE
+column GAME_ID int
+column TILE_X int
+column TILE_Y int
+column ID int
+column DIRECTION string
+
+	// MOVE - represents a user action, i.e a tile rotation
+	var moveStore = database.createObjectStore("MOVE");
+	moveStore.createIndex("SEQUENCE", ["GAME_ID","SEQUENCE"], { unique: true });
+	moveStore.createIndex("TILE", ["TILE_X","TILE_Y"], { unique: false });
+	moveStore.createIndex("ROTATION", "ROTATION", { unique: false });
+
+
+}
+
+//
+ClientStore.prototype.store_new_game = function (game) {
+}
+
+ClientStore.prototype.store_action = function (e) {
+}
+
+ClientStore.prototype.load_game = function (game) {
+}
